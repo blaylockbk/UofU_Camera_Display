d51811544295c3175684e950ecb1afe237316c54
Brian Blaylock <blaylockbk@gmail.com>
2017-01-19 12:25:33 -0700

    New Window! It's just better now. Added bootstrap styling to all the pages

---
+++ CurrentTemp.js
@@ -0,0 +1,12 @@
+document.write("\
+<div align=center style='background-color:black; color:white; font-size:12pt;padding-top: 5px; padding-bottom: 5px;padding-left:5px'>\
+\
+| <a target='new' style='color:white;text-decoration:none;font-family:Segoe UI;' > <span id='ret-tempWBB'>...</span></a> \
+| <a target='new' style='color:white;text-decoration:none;font-family:Segoe UI;' > <span id='ret-tempMTMET'>...</span> </a>\
+| <a target='new' style='color:white;text-decoration:none;font-family:Segoe UI;' > <span id='ret-tempNAA'>...</span> </a>\
+| <a target='new' style='color:white;text-decoration:none;font-family:Segoe UI;' > <span id='ret-tempFPS'>...</span> </a>\
+| <a target='new' style='color:white;text-decoration:none;font-family:Segoe UI;' > <span id='ret-tempFPN'>...</span> </a>\
+| <a target='new' style='color:white;text-decoration:none;font-family:Segoe UI;' > <span id='ret-tempEYSC'>...</span> </a>\
+|\
+</div>\
+");
