3ff025c29d073e667170d75a57bf2a4e281c1059
Brian Blaylock <blaylockbk@gmail.com>
2017-01-27 15:35:44 -0700

    added my Home link

--- siteclose.js
+++ siteclose.js
@@ -1,9 +1,9 @@
 document.write('\
 <div id="footer">\
 <p style="text-align:center; font-family:Segoe UI">\
-This page is maintained by Brian Blaylock &copy 2017, \
+This page is maintained by <a style="color:#990000; text-decoration:none; font-weight:700" href="http://home.chpc.utah.edu/~u0553130/Brian_Blaylock/home.html">Brian Blaylock</a> &copy 2017, \
 Department of Atmospheric Science, University of Utah. \
-Email: <a style="color:#990000; text-decoration:none; font-weight:700" href="mailto:brian.blaylock@utah.edu">brian.blaylock@utah.edu</a>\
+<a style="color:#990000; text-decoration:none; font-weight:700" href="mailto:brian.blaylock@utah.edu"> <i class="fa fa-envelope" aria-hidden="true"></i> brian.blaylock@utah.edu</a>\
     <span style="padding-left:20px;">\
     <a style="color:black;" href="https://github.com/blaylockbk/UofU_Camera_Display" target="_blank">\
         <i class="fa fa-github fa-fw" aria-hidden="true"></i>Get Code on Github\
