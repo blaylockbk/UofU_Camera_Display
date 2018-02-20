59c3f255df4fea7b22929e80fe621e6581ae3e13
Brian Kenneth Blaylock <u0553130@ad.utah.edu>
2018-02-20 10:02:27 -0700

    updated to Bootstrap 4 styling

---
+++ js/siteopen.js
@@ -0,0 +1,16 @@
+document.write('\
+<meta charset="utf-8">\
+<meta name="viewport" content="width=device-width, initial-scale=1">\
+<link type="text/css" rel="stylesheet" type="text/css" href="style.css">\
+<link href="Camera-icon.png" rel="shortcut icon" type="image/png">\
+\
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">\
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>\
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>\
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>\
+\
+<script defer src="https://use.fontawesome.com/releases/v5.0.0/js/all.js"></script>\
+\
+<title>Camera Display</title>\
+<script src="./js/mesowest_api.js"></script>\
+');
