4d55dceedd933a0a2a1192c15119747c5f5579fe
Brian Blaylock <u0553130@kingspeak33.wasatch.peaks>
2020-02-11 16:33:21 -0700

    changed the email address to mesowest list

--- js/siteclose.js
+++ js/siteclose.js
@@ -3,7 +3,7 @@ document.write('\
 <p style="text-align:center; font-family:Segoe UI">\
 This page is maintained by <a style="color:#990000; text-decoration:none; font-weight:700" href="http://home.chpc.utah.edu/~u0553130/Brian_Blaylock/home.html">Brian Blaylock</a> &copy 2019, \
 Department of Atmospheric Science, University of Utah. \
-<a style="color:#990000; text-decoration:none; font-weight:700" href="mailto:brian.blaylock@utah.edu"> <i class="fa fa-envelope"></i> brian.blaylock@utah.edu</a>\
+<a style="color:#990000; text-decoration:none; font-weight:700" href="mailto:atmos-mesowest@lists.utah.edu"> <i class="fa fa-envelope"></i> atmos-mesowest@lists.utah.edu </a>\
     <span style="padding-left:20px;">\
     <a style="color:black;" href="https://github.com/blaylockbk/UofU_Camera_Display" target="_blank">\
     <i class="fab fa-github"></i> Get Code on Github\
